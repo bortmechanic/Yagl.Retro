4b3d978e8ed5803fb4411d62ce37a5e8b3d843c0
Pavel Melnikov <pavel.v.melnikov@gmail.com>
2020-10-04 19:45:05 +0300

    Designer project for the Win platform.

---
+++ Designer/Win/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Navigation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace Win
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Forms.Init();
+            LoadApplication(new Yagl.Retro.Designer.App());
+        }
+    }
+}
