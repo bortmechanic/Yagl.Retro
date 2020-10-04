05b8ec5e16335ac9708351dac864f5b2d784d699
pavel <pavel.v.melnikov@gmail.com>
2020-10-04 18:33:19 +0300

    Designer project for the Mac platform.

---
+++ Designer/Platforms/Mac/AppDelegate.cs
@@ -0,0 +1,38 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace Yagl.Retro.Designer.Mac
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        public AppDelegate()
+        {
+            const NSWindowStyle style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+            var rect = new CoreGraphics.CGRect(200, 50, 800, 600);
+            MainWindow = new NSWindow(rect, style, NSBackingStore.Buffered, false)
+            {
+                Title = "YAGL Retro Designer", 
+                TitleVisibility = NSWindowTitleVisibility.Hidden
+            };
+        }
+        
+        public override NSWindow MainWindow { get; }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        /*
+        public override void WillTerminate(NSNotification notification)
+        {
+            base.WillTerminate(notification);
+        }
+        */
+    }
+}
---
+++ Designer/Platforms/Mac/Main.cs
@@ -0,0 +1,14 @@
+﻿using AppKit;
+
+namespace Yagl.Retro.Designer.Mac
+{
+    internal static class MainClass
+    {
+        private static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate();
+            NSApplication.Main(args);
+        }
+    }
+}
