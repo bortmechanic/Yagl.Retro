432b6cab641ec6365f405d684581e55e896e2c96
pavel <pavel.v.melnikov@gmail.com>
2020-10-04 18:23:57 +0300

    Xamaring project for designer.

---
+++ Designer/App/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Yagl.Retro.Designer
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapButton_OnClicked(object sender, EventArgs e)
+        {
+            HelloLabel.Text += " Clicked!";
+        }
+    }
+}
